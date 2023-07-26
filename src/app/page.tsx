db596438e9fd05e4bb66cf6fd07e2ff1f9faebcd
Edward Tanguay (windows) <edward@tanguay.info>
2023-07-26 16:42:04 +0200

    showing sql

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
-export default function Home() {
-	return <h1>The Todo App</h1>
+import { prisma } from '@/db';
+import Link from 'next/link';
+
+export default async function Home() {
+	const todos = await prisma.todo.findMany();
+
+	return (
+		<>
+			<header className="flex justify-between items-center mb-4">
+				<h1 className="text-2xl">Todos</h1>
+				<Link
+					className="border border-slate-300 text-slate-300 px-1 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+					href="/info"
+				>
+					Info
+				</Link>
+			</header>
+			<ul className="">
+				{todos.map((todo) => (
+					<li key={todo.id}>{todo.title}</li>
+				))}
+			</ul>
+		</>
+	);
 }
