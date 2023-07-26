a9df61e4a4104d4e46409227eaebe4d422b5eac2
Edward Tanguay (windows) <edward@tanguay.info>
2023-07-26 19:29:11 +0200

    checkbox works

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,10 +1,18 @@
 interface ITodo {
+	id: string;
 	title: string;
 }
 type IProps = {
 	todo: ITodo;
-}
+};
 
 export function TodoItem({ todo }: IProps) {
-	return <li>{todo.title}</li>;
+	return (
+		<li className="flex gap-1 items-center">
+			<input id={todo.id} type="checkbox" className="cursor-pointer peer"/>
+			<label htmlFor={todo.id} className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500">
+				{todo.title}
+			</label>
+		</li>
+	);
 }
